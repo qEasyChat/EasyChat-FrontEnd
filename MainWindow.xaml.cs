4261c6e74ef2f440d96d9aebbdf3211e62cf88fe
q1e123 <robertnicolaesolca@gmail.com>
2021-12-09 09:25:21 +0200

    added dll imports and select db file

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +13,11 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
+using System.Drawing;
+using System.IO;
+using System.Security;
+
 
 namespace EasyChat_FrontEnd
 {
@@ -20,9 +26,22 @@ namespace EasyChat_FrontEnd
     /// </summary>
     public partial class MainWindow : Window
     {
+        [DllImport("EasyChat-DLLs.dll", CallingConvention = CallingConvention.Cdecl)]
+        extern static int new_server(int x);
+        [DllImport("EasyChat-DLLs.dll", CallingConvention = CallingConvention.Cdecl)]
+        extern static int get_driver_type(string db_type);
+        [DllImport("EasyChat-DLLs.dll", CallingConvention = CallingConvention.Cdecl)]
+        extern static int connect_to_database(IntPtr server,int driver_type, string file_path);
+
+        [DllImport("EasyChat-DLLs.dll", CallingConvention = CallingConvention.Cdecl)]
+        extern static int start(IntPtr server);
+
+        private OpenFileDialog openFileDialog;
+
         public MainWindow()
         {
             InitializeComponent();
+            openFileDialog = new OpenFileDialog();
         }
 
         private void runServerButton_Click(object sender, RoutedEventArgs e)
@@ -34,6 +53,32 @@ namespace EasyChat_FrontEnd
         private void Image_MouseDown(object sender, MouseButtonEventArgs e)
         {
             System.Diagnostics.Process.Start("https://github.com/qEasyChat");
+        }
+
+        private void startServerButton_Click(object sender, RoutedEventArgs e)
+        {
+            string serverName = serverNameTextBox.Text;
+            string portString = portNumberTextBox.Text;
+            int port = Int32.Parse(portString)
+            IntPtr server = new_server();
+        }
+
+        private void selectDBFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (openFileDialog.ShowDialog() == true)
+            {
+                try
+                {
+                    string filepath = openFileDialog.FileName;
+                    MessageBox.Show(filepath);
+                }
+                catch (SecurityException ex)
+                {
+                    MessageBox.Show($"Security error.\n\nError message: {ex.Message}\n\n" +
+                                    $"Details:\n\n{ex.StackTrace}");
+                }
+            }
+
         }
     }
 }
